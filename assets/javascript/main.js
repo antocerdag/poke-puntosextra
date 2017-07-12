c368b9b4b989fcb5709931802575880df99db859
Antonia Cerda <antoniacerdag@gmail.com>
2017-07-12 17:12:37 -0400

    agregando css

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -36,7 +36,6 @@ var habilidades = function(url){
 	})
 	.done(function(respuesta) {
 		console.log(respuesta);
-		
 	})
 	.fail(function() {
 		console.log("error");
@@ -57,9 +56,8 @@ $.ajax({
 	respuesta.results.forEach(function(el,img){
 	var pokeName = el.name;
 	var img = img + 1;
-	var habilidades = habilidades(el.url);
-	$(".caja-pokemon").append("<a class='waves-effect waves-light btn boton'><div class='pokelinea1'><img class='pokeimg' src='http://pokeapi.co/media/img/"+img+ ".png'><span class='namePoke text-center'>"+pokeName+"</span>"+"<span class='habilidades'></span></div> </a>");
-	$(".habilidades").append(habilidades);
+	habilidades(el.url);
+	$(".caja-pokemon").append("<a class='waves-effect waves-light btn boton'><div class='pokelinea1'><img class='pokeimg' src='http://pokeapi.co/media/img/"+img+ ".png'><span class='namePoke text-center'>"+pokeName+"</span>"+habilidades(el.url)+"<span class='habilidades'></span></div> </a>");
 	})
 
 	
